e0eab376ee0641d30227709c83826cc265b974ef
Purplebelt1 <wdeb2013@gmail.com>
2024-11-13 04:14:30 -0600

    Added Data Annotations

--- DropWeightBackend.Domain/Entities/Workout.cs
+++ DropWeightBackend.Domain/Entities/Workout.cs
@@ -8,8 +8,14 @@ namespace DropWeight.Domain.Entities
     {
         [Key]
         public int WorkoutId { get; set; }
+
+        [Required]
         public int StartTime { get; set; }
+
+        [Required]
         public int EndTime { get; set; }
+        
+        [Required]
         public WorkoutType Type { get; set; }
         public int Reps { get; set; }
         public ICollection<GeoSpatial> GeoSpatials { get; set;}
