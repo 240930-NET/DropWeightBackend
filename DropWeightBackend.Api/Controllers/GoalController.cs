9cbee1835b96793bcda49e0204297d3da14acd0a
Gbodson <gbodson02@gmail.com>
2024-11-27 11:19:14 -0500

    afix

--- DropWeightBackend.Api/Controllers/GoalController.cs
+++ DropWeightBackend.Api/Controllers/GoalController.cs
@@ -4,88 +4,88 @@ using DropWeightBackend.Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DropWeightBackend.Api.Controllers;
+namespace DropWeightBackend.Api.Controllers {
 
 
-[ApiController]
-[Route("api/[controller]")]
-public class GoalController : ControllerBase {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GoalController : ControllerBase {
 
-    public readonly IGoalService _goalService;
+        public readonly IGoalService _goalService;
 
-    public GoalController(IGoalService goalService) {
-        _goalService = goalService;
-    }
-
-
-    //[Authorize]
-    [HttpGet]
-    public async Task<IActionResult> GetAllGoals() {
-
-        try {
-            return Ok(await _goalService.GetAllGoals());
+        public GoalController(IGoalService goalService) {
+            _goalService = goalService;
         }
-        catch(Exception e) {
-            return BadRequest(e.Message);
-        }
-    }
 
-    
-    //[Authorize]
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetGoalById(int id) {
-        try {
-            var goal = await _goalService.GetGoalById(id);
-            if (goal == null)
-            {
-                return NotFound($"Goal with id: {id} not found.");
+
+        //[Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetAllGoals() {
+
+            try {
+                return Ok(await _goalService.GetAllGoals());
             }
-            return Ok(await _goalService.GetGoalById(id));
+            catch(Exception e) {
+                return BadRequest(e.Message);
+            }
         }
-        catch(Exception e) {
-            return BadRequest(e.Message);
+
+        
+        //[Authorize]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetGoalById(int id) {
+            try {
+                var goal = await _goalService.GetGoalById(id);
+                if (goal == null)
+                {
+                    return NotFound($"Goal with id: {id} not found.");
+                }
+                return Ok(await _goalService.GetGoalById(id));
+            }
+            catch(Exception e) {
+                return BadRequest(e.Message);
+            }
+        }
+
+
+        //[Authorize]
+        [HttpPost]
+        public async Task<IActionResult> AddGoal([FromBody] GoalDto goalDTO) {
+
+            try {
+                await _goalService.AddGoal(goalDTO);
+                return Ok(goalDTO);
+            }
+            catch(Exception e) {
+                Console.WriteLine("catch:" + goalDTO);
+                return BadRequest(e.Message);
+            }
+        }
+
+
+        //[Authorize]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateGoal([FromBody] GoalDto goalDTO, int id) {
+            try {
+                await _goalService.UpdateGoal(goalDTO, id);
+                return Ok(goalDTO);
+            }
+            catch(Exception e) {
+                return BadRequest(e.Message);
+            }
+        }
+
+
+        [Authorize]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGoal(int id) {
+            try {
+                await _goalService.DeleteGoal(id);
+                return Ok();
+            }
+            catch(Exception e) {
+                return BadRequest(e.Message);
+            }
         }
     }
-
-
-    //[Authorize]
-    [HttpPost]
-    public async Task<IActionResult> AddGoal([FromBody] GoalDto goalDTO) {
-
-        try {
-            await _goalService.AddGoal(goalDTO);
-            return Ok(goalDTO);
-        }
-        catch(Exception e) {
-            Console.WriteLine("catch:" + goalDTO);
-            return BadRequest(e.Message);
-        }
-    }
-
-
-    //[Authorize]
-    [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateGoal([FromBody] GoalDto goalDTO, int id) {
-        try {
-            await _goalService.UpdateGoal(goalDTO, id);
-            return Ok(goalDTO);
-        }
-        catch(Exception e) {
-            return BadRequest(e.Message);
-        }
-    }
-
-
-    [Authorize]
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteGoal(int id) {
-        try {
-            await _goalService.DeleteGoal(id);
-            return Ok();
-        }
-        catch(Exception e) {
-            return BadRequest(e.Message);
-        }
-    }
-
 }
